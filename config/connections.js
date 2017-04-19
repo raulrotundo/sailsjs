b32b6326ea3f56b2da89f0fa859ae49d5bef66ed
raul_personal_laptop <raulrotundo@gmail.com>
2017-04-18 22:02:13 -0300

    Update to mongoDB adapter

--- config/connections.js
+++ config/connections.js
@@ -28,9 +28,9 @@ module.exports.connections = {
   * Installed by default.                                                    *
   *                                                                          *
   ***************************************************************************/
-  localDiskDb: {
-    adapter: 'sails-disk'
-  },
+  // localDiskDb: {
+  //   adapter: 'sails-disk'
+  // },
 
   /***************************************************************************
   *                                                                          *
@@ -57,9 +57,9 @@ module.exports.connections = {
   *                                                                          *
   ***************************************************************************/
   // someMongodbServer: {
-  //   adapter: 'sails-mongo',
-  //   host: 'localhost',
-  //   port: 27017,
+    adapter: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
   //   user: 'username', //optional
   //   password: 'password', //optional
   //   database: 'your_mongo_db_name_here' //optional
