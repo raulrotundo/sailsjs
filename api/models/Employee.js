c1e70d5b3a112a0d4267b57a32a63019f7900996
raul_personal_laptop <raulrotundo@gmail.com>
2017-04-19 22:28:14 -0300

    Remove employee id model

--- api/models/Employee.js
+++ api/models/Employee.js
@@ -7,11 +7,6 @@
 
 module.exports = {
     attributes: {
-        id: {
-            type: "string",
-            required: true,
-            unique: true
-        },
         name: {
             type: "string",
             required: true,
